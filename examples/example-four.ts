c4701d0c0cada90ad24804f1e068c6d0e16d7793
Aaron Bos <aaronmbos@gmail.com>
2022-04-15 19:58:18 -0500

    Finish lessons 23, 24, 25

--- examples/example-four.ts
+++ examples/example-four.ts
@@ -9,18 +9,27 @@ type Conference = TechEventBase & {
   location: string;
   price: number;
   talks: Talk[];
+  kind: "conference";
 };
 
 type Meetup = TechEventBase & {
   location: string;
   price: string;
   talks: Talk[];
+  kind: "meetup";
 };
 
 type Webinar = TechEventBase & {
   url: string;
   price?: number;
   talks: Talk;
+  kind: "webinar";
+};
+
+type Hackathon = TechEventBase & {
+  location: string;
+  price?: number;
+  kind: "hackathon";
 };
 
 type TechEventBase = {
@@ -29,7 +38,42 @@ type TechEventBase = {
   date: Date;
   capacity: number;
   rsvp: number;
-  kind: string;
 };
 
-type TechEvent = Webinar | Conference | Meetup;
+type TechEvent = Webinar | Conference | Meetup | Hackathon;
+
+function getEventTeaser(event: TechEvent) {
+  switch (event.kind) {
+    case "conference":
+      return `${event.title} (Conference)`;
+    case "meetup":
+      return `${event.title} (Meetup)`;
+    case "webinar":
+      return `${event.title} (Webinar)`;
+  }
+}
+
+type EventKind = "conference" | "webinar" | "meetup";
+
+function filterByKind(list: TechEvent[], kind: EventKind): TechEvent[] {
+  return list.filter((el) => el.kind === kind);
+}
+
+type GroupedEvents = {
+  [Kind in EventKind]: TechEvent[];
+};
+
+function groupEvents(events: TechEvent[]): GroupedEvents {
+  const grouped = {
+    conference: [],
+    meetup: [],
+    webinar: [],
+    hackathon: [],
+  };
+
+  events.forEach((el) => {
+    grouped[el.kind].push(el);
+  });
+
+  return grouped;
+}
