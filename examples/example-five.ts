9c22204cfff195e3f0c1deb3fdd4f6c9a56fe8f5
Aaron Bos <aaronmbos@gmail.com>
2022-04-21 21:43:55 -0500

    Finish lessons 30, 31, 32

---
+++ examples/example-five.ts
@@ -0,0 +1,26 @@
+function isAvailable<Formats>(
+  obj: Formats,
+  key: string | number | symbol
+): key is keyof Formats {
+  return key in obj;
+}
+
+type UrlObject = {
+  [k: string]: URL;
+};
+
+type Loaded<Key> = {
+  format: Key;
+  loaded: boolean;
+};
+
+async function loadFile<Formats extends UrlObject, Key extends keyof Formats>(
+  fileFormats: Formats,
+  format: Key
+): Promise<Loaded<Key>> {
+  const data = await fetch(fileFormats[format].href);
+  return {
+    format,
+    loaded: data.status === 200,
+  };
+}
